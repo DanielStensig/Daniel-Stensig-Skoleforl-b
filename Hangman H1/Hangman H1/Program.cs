a5ec2c1349699ba159f50b7ac418630410a7f286
DanielStensig <146441666+DanielStensig@users.noreply.github.com>
2023-11-20 08:48:33 +0100

    Added Hangman

---
+++ Hangman H1/Hangman H1/Program.cs
@@ -0,0 +1,65 @@
+﻿Main();
+static void Main()
+{
+    string randomWord = "";
+    RandomArray(out randomWord);
+    Console.WriteLine("Random word: " + randomWord);
+
+    while (true)
+    {
+        GuessMethod(randomWord);
+    }
+
+}
+static void RandomArray(out string randomWord)
+{
+    string[] RandomWords = new string[] { "KILLING", "HVALP", "HAMSTER", "KANIN" };
+
+    Random random = new Random(); // Her laver jeg en ny random
+
+    int randomIndex = random.Next(RandomWords.Length); // Laver et random index i dette arrays bounds
+
+    randomWord = RandomWords[randomIndex]; // Vælger 1 random ord
+}
+static void GuessMethod(string randomWord)
+{
+    string guess;
+
+    Console.Write("\nIndtast dit gæt: ");
+    guess = Console.ReadLine().ToUpper();
+
+    if (guess.Length == 1 && char.IsLetter(guess[0]))
+    {
+
+        if (randomWord.Contains(guess))
+        {
+            char[] showWord = new char[randomWord.Length];
+
+            for (int i = 0; i < randomWord.Length; i++)
+            {
+                if (randomWord[i] == guess[0])
+                {
+                    showWord[i] = guess[0];
+                }
+                else if (char.IsLetter(showWord[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    showWord[i] = '_';
+                }
+            }
+            Console.WriteLine("Ordet: " + new string(showWord));
+        }
+        else
+        {
+            Console.WriteLine("Forkert");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Du må kun indtaste 1 BOGSTAV af gangen: ");
+        Console.ReadKey();
+    }
+}
