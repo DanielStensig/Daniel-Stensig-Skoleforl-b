4105ef9f651288955565ed6ba7b58c93199857f6
DanielStensig <146441666+DanielStensig@users.noreply.github.com>
2023-12-13 14:38:02 +0100

    Add files via upload

---
+++ Exception Handeling H1/Exception Handeling H1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exception_Handeling_H1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program start: ");
+
+            int tal1 = 7;
+            int tal2 = 0;
+            int tal3 = 0;
+            try
+            {
+                tal3 = int.Parse("Halvtreds");
+                tal3 = tal1 / tal2;
+                Console.WriteLine("Slut af try");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Det gik ikke så godt");
+                Console.WriteLine(e.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("All other exceptions");
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+
+            }
+            Console.WriteLine(tal3);
+
+            Console.WriteLine("Program slut: ");
+
+            Console.ReadLine();
+        }
+    }
+}
