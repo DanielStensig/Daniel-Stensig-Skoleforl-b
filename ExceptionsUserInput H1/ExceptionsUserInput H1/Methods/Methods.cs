668441551f056046d33cd2f0e2c8866de9a882f1
DanielStensig <146441666+DanielStensig@users.noreply.github.com>
2023-12-13 14:36:08 +0100

    Add files via upload

---
+++ ExceptionsUserInput H1/ExceptionsUserInput H1/Methods/Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsUserInput_H1.Classes
+{
+    public class Methods
+    {
+        public int GetIntFromUserException(string text)
+        {
+            while (true)
+            {
+                Console.WriteLine(text);
+                string input = Console.ReadLine();
+                try
+                {
+                    return int.Parse(input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Det virkede ikke");
+                }
+            }
+        }
+        public int GetIntFromUserTryParse(string text)
+        {
+            while (true)
+            {
+                Console.WriteLine(text);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int result))
+                {
+                    Console.WriteLine("Det virkede");
+                }
+                else
+                {
+                    Console.WriteLine("Indtast en integer");
+                    Console.WriteLine("Det virkede ikke");
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ ExceptionsUserInput H1/ExceptionsUserInput H1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExceptionsUserInput_H1.Classes;
+namespace ExceptionsUserInput_H1
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            int tal5 = methods.GetIntFromUserException("Indtast venligst et heltal:");
+            Console.WriteLine("Tak for det. Der blev tastet " + tal5);
+
+            int tal6 = methods.GetIntFromUserTryParse("Indtast venligst et heltal:");
+            Console.WriteLine("Tak for det. Der blev tastet " + tal6);
+            Console.ReadLine();
+        }
+    }
+}
