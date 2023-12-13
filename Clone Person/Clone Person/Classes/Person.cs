3ed21dc2f02adf399026724bf59bd44e2e7e4ad3
DanielStensig <146441666+DanielStensig@users.noreply.github.com>
2023-12-13 14:31:59 +0100

    Add files via upload

---
+++ Clone Person/Clone Person/Classes/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clone_Person.Classes
+{
+    internal class Person
+    {
+        private string _name;
+        private int _height;
+        private int _weight;
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Height { get { return _height; } set { _height = value; } }
+        public int Weight { get { return _weight; } set { _weight = value; } }
+
+        public Person(string name, int heigt, int weight) 
+        { 
+            _name = name;
+            _height = heigt;
+            _weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {_name}, Height: {_height}, Weight: {_weight}";
+        }
+
+    }
+}
---
+++ Clone Person/Clone Person/Program.cs
@@ -0,0 +1,20 @@
+﻿using Clone_Person.Classes;
+
+namespace Clone_Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new("Daniel Stensig", 180, 75);
+            Person p2 = p1;
+
+            Console.WriteLine("Er det det samme: " + ReferenceEquals(p1, p2));
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            p1.Height = 100;
+            Console.WriteLine("Er det det samme: " + ReferenceEquals(p1, p2));
+        }
+    }
+}
